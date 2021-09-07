a53a9314db1c011198ebe1fd983ca29d82fab776
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-09-07 14:31:42 +0200

    minor fixes

--- ktkq.py
+++ ktkq.py
@@ -1,10 +1,10 @@
 import numpy as np
+import vtk
 
 
-def t(smithers_time_dict):
-    faces_dict = smithers_time_dict["boundary"][b"propellerTip"]["faces"]
+def field_on_boundary(smithers_time_dict, boundary_key, field_key):
+    faces_dict = smithers_time_dict["boundary"][boundary_key]["faces"]
     face_idxes = faces_dict["faces_indexes"]
-    face_areas = faces_dict["area"]
     face_normals = faces_dict["normal"]
 
     # the index of the cell which owns each face which belongs to propellerTip
@@ -14,10 +14,18 @@ def t(smithers_time_dict):
 
     # we use the index of the cell to obtain the value of the pressure at
     # the center of that cell, for each cell which touches the boundary
-    pressure = np.asarray(smithers_time_dict["fields"]["p"][1])[
+    return np.asarray(smithers_time_dict["fields"][field_key][1])[
         outer_cells_labels
     ]
 
+
+def normals_to_boundary(smithers_time_dict, boundary_key):
+    return smithers_time_dict["boundary"][boundary_key]["faces"]["normal"]
+
+
+def t(smithers_time_dict):
+    pressure = field_on_boundary(smithers_time_dict, b'propellerTip', 'p')
+    face_normals = normals_to_boundary(smithers_time_dict, b'propellerTip')
     return np.sum(pressure[:, None] * face_normals, axis=0)
 
 
@@ -25,3 +33,4 @@ def t_time_progression(smithers_dict):
     return [
         t(smithers_dict[time]) for time in sorted(list(smithers_dict.keys()))
     ]
+
