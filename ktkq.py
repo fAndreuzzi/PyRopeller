61cbaacf43c3b489f5bc16a27f8c6389ed07b7ca
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-09-07 22:22:58 +0200

    triangulate mesh before

--- ktkq.py
+++ ktkq.py
@@ -1,36 +1,49 @@
 import numpy as np
-import vtk
+from smithers.io.vtkhandler import VTKHandler
+from vtk import vtkPolyDataReader, vtkPolyDataWriter, vtkTriangleFilter
+import os
 
 
-def field_on_boundary(smithers_time_dict, boundary_key, field_key):
-    faces_dict = smithers_time_dict["boundary"][boundary_key]["faces"]
-    face_idxes = faces_dict["faces_indexes"]
-    face_normals = faces_dict["normal"]
+def normals(vtk_data):
+    def nrm(p1, p2, p3):
+        v1 = vtk_data["points"][p2] - vtk_data["points"][p1]
+        v2 = vtk_data["points"][p3] - vtk_data["points"][p1]
+        return np.cross(v1, v2)
 
-    # the index of the cell which owns each face which belongs to propellerTip
-    outer_cells_labels = list(
-        map(smithers_time_dict["face_owner_cell"].__getitem__, face_idxes)
+    return list(map(lambda tp: nrm(*tp), vtk_data["cells"]))
+
+
+def triangulate_vtk(vtk_path):
+    handler = vtkPolyDataReader()
+    handler.SetFileName(vtk_path)
+    handler.Update()
+    polydata = handler.GetOutput()
+
+    filter = vtkTriangleFilter()
+    filter.SetInputData(polydata)
+    filter.Update()
+    return filter.GetOutput()
+
+
+def t(vtk_path):
+    handler = VTKHandler(vtkPolyDataReader, vtkPolyDataWriter)
+    triangulated_vtk_data = triangulate_vtk(vtk_path)
+    data = handler.parse(triangulated_vtk_data)
+
+    pressure = data["cell_data"]["p"]
+    n = normals(data)
+
+    return np.sum(pressure[:, None] * n, axis=0)
+
+
+def t_time_progression(vtk_folder_path):
+    return list(
+        map(
+            t,
+            # we add the leading part to make the path absolute
+            map(
+                lambda s: vtk_folder_path + "/" + s,
+                os.listdir(vtk_folder_path),
+            ),
+        )
     )
-
-    # we use the index of the cell to obtain the value of the pressure at
-    # the center of that cell, for each cell which touches the boundary
-    return np.asarray(smithers_time_dict["fields"][field_key][1])[
-        outer_cells_labels
-    ]
-
-
-def normals_to_boundary(smithers_time_dict, boundary_key):
-    return smithers_time_dict["boundary"][boundary_key]["faces"]["normal"]
-
-
-def t(smithers_time_dict):
-    pressure = field_on_boundary(smithers_time_dict, b'propellerTip', 'p')
-    face_normals = normals_to_boundary(smithers_time_dict, b'propellerTip')
-    return np.sum(pressure[:, None] * face_normals, axis=0)
-
-
-def t_time_progression(smithers_dict):
-    return [
-        t(smithers_dict[time]) for time in sorted(list(smithers_dict.keys()))
-    ]
-
