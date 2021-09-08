e1f8c3e8de0cb356cf530a697ab5d7729cc4aa41
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-09-08 14:34:09 +0200

    return np.ndarray instead of tuple

--- ktkq.py
+++ ktkq.py
@@ -12,7 +12,7 @@ def normals_and_area(vtk_data):
         n = np.cross(v1, v2)
         norm = np.linalg.norm(n)
 
-        return (n / norm, abs(norm) / 2)
+        return np.array([n / norm, abs(norm) / 2])
 
     return np.apply_along_axis(nrm, 1, vtk_data['cells'])
 
