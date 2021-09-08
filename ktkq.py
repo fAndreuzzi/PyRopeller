ef7b5d7387203b5dfcb79158656a0a1991d8a596
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-09-08 13:58:59 +0200

    include area in the integration

--- ktkq.py
+++ ktkq.py
@@ -4,12 +4,16 @@ from vtk import vtkPolyDataReader, vtkPolyDataWriter, vtkTriangleFilter
 import os
 
 
-def normals(vtk_data):
+def normals_and_area(vtk_data):
     def nrm(p1, p2, p3):
         v1 = vtk_data["points"][p2] - vtk_data["points"][p1]
         v2 = vtk_data["points"][p3] - vtk_data["points"][p1]
+
         n = np.cross(v1, v2)
-        return n / np.linalg.norm(n)
+        norm = np.linalg.norm(n)
+
+        return (n / norm, abs(norm) / 2)
+
     return np.array(list(map(lambda tp: nrm(*tp), vtk_data["cells"])))
 
 
@@ -30,9 +34,11 @@ def t(vtk_path):
     data = handler.parse(triangulated_vtk_data)
 
     pressure = data["cell_data"]["p"]
-    n = normals(data)
+    na = normals_and_area(data)
+    n = na[:,0]
+    a = na[:,1]
 
-    return np.sum(pressure[:, None] * n, axis=0)
+    return np.sum(pressure[:, None] * n * a, axis=0)
 
 
 def t_time_progression(vtk_folder_path):
